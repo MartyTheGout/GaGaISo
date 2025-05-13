4e60d982ca20394e75192a16364c9e9c0f5c7faf
MartyTheGout <chihyung0@naver.com>
2025-05-13 16:24:30 +0900

    Initial Commit

---
+++ GaGaISo/GaGaISoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaGaISoApp.swift
+//  GaGaISo
+//
+//  Created by marty.academy on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GaGaISoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
