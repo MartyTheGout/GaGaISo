fd2599fec2e942ad8c7d19790e943e2b60446f4a
MartyTheGout <chihyung0@naver.com>
2025-05-29 20:01:36 +0900

    [Network] Router + DTO for Community

---
+++ GaGaISo/Data/Network/DTO/CommunityPostsDTO.swift
@@ -0,0 +1,62 @@
+//
+//  CommunityPostsDTO.swift
+//  GaGaISo
+//
+//  Created by marty.academy on 5/29/25.
+//
+
+import Foundation
+
+
+struct PostsDTO: Codable {
+    let data: [PostDTO]
+    let nextCursor: String
+
+    enum CodingKeys: String, CodingKey {
+        case data
+        case nextCursor = "next_cursor"
+    }
+}
+
+struct PostDTO: Codable {
+    let postID, category, title, content: String
+    let store: Store
+    let geolocation: Geolocation
+    let creator: Creator
+    let files: [String]
+    let isLike: Bool
+    let likeCount: Int
+    let createdAt, updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case postID = "post_id"
+        case category, title, content, store, geolocation, creator, files
+        case isLike = "is_like"
+        case likeCount = "like_count"
+        case createdAt, updatedAt
+    }
+}
+
+struct Store: Codable {
+    let id, category, name, close: String
+    let storeImageUrls: [String]
+    let isPicchelin, isPick: Bool
+    let pickCount: Int
+    let hashTags: [String]
+    let totalRating, totalOrderCount, totalReviewCount: Int
+    let geolocation: Geolocation
+    let createdAt, updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, category, name, close
+        case storeImageUrls = "store_image_urls"
+        case isPicchelin = "is_picchelin"
+        case isPick = "is_pick"
+        case pickCount = "pick_count"
+        case hashTags
+        case totalRating = "total_rating"
+        case totalOrderCount = "total_order_count"
+        case totalReviewCount = "total_review_count"
+        case geolocation, createdAt, updatedAt
+    }
+}
--- GaGaISo/Data/Network/Router/CommunityRouter.swift
+++ GaGaISo/Data/Network/Router/CommunityRouter.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
+enum CommunityOrderCriteria: String {
+    case createdAt
+    case likes
+}
+
 enum CommunityRouter: RouterProtocol {
-    
-    case v1AuthRefresh(accessToken: String, refreshToken: String)
-    
+    case v1PostFiles(files: [String])
+    case v1Posts(category: String, title: String, content: String, storeId: String, latitude: Double, longitude: Double, file: [String])
+    case v1GetPost(category: String, latitude: Double, longitude: Double, maxDistance: String, limit: Int, next: String, orderBy: CommunityOrderCriteria)
+    case v1SearchPost(title: String)
+    case v1GetPostDetail(postId: String)
+    case v1ModifyPost(postId: String, category: String, title: String, content: String, storeId: String, latitude: Double, longitude: Double, file: [String])
+    case v1DeletePost(postId: String)
+    case v1LikePost(postId: String, status: Bool)
+    case v1GetPostBasedOn(userId: String, category: String, limit: Int, next: String)
+    case v1GetPostLikedByMe(category: String, next: String, limit: Int)
+
     var baseURL: URL {
         guard let url = URL(string: ExternalDatasource.pickup.baseURLString) else {
             fatalError("[Error: Router] Couldn't find baseURL error")
@@ -20,27 +33,92 @@ enum CommunityRouter: RouterProtocol {
     
     var path: String {
         switch self {
-        case .v1AuthRefresh : return "v1/auth/refresh"
-            
+        case .v1PostFiles: return "v1/posts/files"
+        case .v1Posts: return "v1/posts"
+        case .v1GetPost: return "v1/posts/geolocation"
+        case .v1SearchPost: return "v1/posts/search"
+        case .v1GetPostDetail(let postId) : return "v1/posts/\(postId)"
+        case .v1ModifyPost(let postId, _, _, _, _, _, _,_) : return "v1/posts/\(postId)"
+        case .v1DeletePost(let postId) : return "v1/posts/\(postId)"
+        case .v1LikePost(let postId, _) : return "v1/posts/\(postId)/like"
+        case .v1GetPostBasedOn(let userId, _, _, _)  : return "v1/posts/users/\(userId)"
+        case .v1GetPostLikedByMe: return "v1/posts/likes/me"
         }
     }
     
     var parameter : [URLQueryItem] {
         switch self {
+        case .v1GetPost(let category, let latitude, let longitude,let maxDistance,let limit, let next, let orderBy):
+            return [
+                .init(name: "category", value: category),
+                .init(name: "latitude", value: "\(latitude)"),
+                .init(name: "longitude", value: "\(longitude)"),
+                .init(name: "maxDistance", value: maxDistance),
+                .init(name: "next", value: next),
+                .init(name: "orderBy", value: orderBy.rawValue)
+            ]
+        case .v1SearchPost(let title):
+            return [
+                .init(name: "title", value: title)
+            ]
+        case .v1GetPostBasedOn(_, let category, let limit, let next), .v1GetPostLikedByMe(let category, let next, let limit):
+            return [
+                .init(name: "category", value: category),
+                .init(name: "next", value: next),
+                .init(name: "limit", value: "\(limit)"),
+            ]
         default: return []
         }
     }
+    
     var body: Data? {
         switch self {
-        case .v1AuthRefresh:
+        case .v1PostFiles:
             return nil
+            
+        case .v1Posts(let category, let title, let content, let storeId, let latitude, let longitude, let files):
+            let dict = [
+                "category": category,
+                "title": title,
+                "content": content,
+                "store_id": storeId,
+                "latitude": latitude,
+                "longitude": longitude,
+                "files": files
+            ] as [String : Any]
+            return try? JSONSerialization.data(withJSONObject: dict)
+            
+        case .v1ModifyPost(let postId, let category, let title, let content, let storeId, let latitude, let longitude, let files):
+            let dict = [
+                "category": category,
+                "title": title,
+                "content": content,
+                "store_id": storeId,
+                "latitude": latitude,
+                "longitude": longitude,
+                "files": files
+            ] as [String : Any]
+            return try? JSONSerialization.data(withJSONObject: dict)
+            
+        case .v1LikePost(_, let isLike):
+            let dict = [
+                "like_status": isLike
+            ] as [String: Any]
+            return try? JSONSerialization.data(withJSONObject: dict)
+            
+        default: return nil
         }
     }
     
     var method: String {
         switch self {
-        case .v1AuthRefresh:
-            return "GET"
+        case .v1PostFiles, .v1Posts, .v1LikePost:
+            return "POST"
+        case .v1DeletePost:
+            return "DELETE"
+        case .v1ModifyPost:
+            return "PUT"
+        default: return "GET"
         }
     }
 }
