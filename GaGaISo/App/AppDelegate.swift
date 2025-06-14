a96ffdd40c8a6732387d29137d30147ca4d29a4e
MartyTheGout <chihyung0@naver.com>
2025-06-14 22:27:42 +0900

    [Feature] Push notification, dependency + basic push handling added

--- GaGaISo/App/AppDelegate.swift
+++ GaGaISo/App/AppDelegate.swift
@@ -6,21 +6,58 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     var authStore: AbstractAuthStore?
     
     func application(_ application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let token = deviceToken.map { String(format: "%02x", $0) }.joined()
-        print("Successfully Device Token Register: \(token)")
-        
-        authStore?.deviceToken = token
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        setupFCM(application)
+        return true
     }
+    
+    private func setupFCM(_ application: UIApplication) {
+           Messaging.messaging().delegate = self
+           UNUserNotificationCenter.current().delegate = self
+           UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { isAgree, error in
+               if isAgree {
+                   print("알림허용")
+               }
+           }
+           application.registerForRemoteNotifications()
+       }
+}
 
-    func application(_ application: UIApplication,
-                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Failed to Register Device Token: \(error.localizedDescription)")
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    /// 푸시클릭시
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        print("🟢", #function)
+    }
+    
+    /// 앱화면 보고있는중에 푸시올 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        print("🟢", #function)
+        return [.sound, .banner, .list]
+    }
+    
+    /// FCMToken 업데이트시
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("🟢", #function, fcmToken)
+    }
+    
+    /// 스위즐링 NO시, APNs등록, 토큰값가져옴
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print("🟢", #function, deviceTokenString)
+    }
+    
+    /// error발생시
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("🟢", error)
     }
 }
